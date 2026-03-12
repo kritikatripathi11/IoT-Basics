0929270ca551e538fde23db096cb9f5695feeaf4
kritikatripathi11 <tripathikritika44@gmail.com>
2026-03-12 10:19:31 +0530

    Add files via upload

---
+++ Iot_code2.ino
@@ -0,0 +1,32 @@
+#define my_led 7
+#define led 2
+#define u_led 4
+void setup (){
+ Serial.begin(9600);
+ pinMOde(my_led, OUTPUT);
+ pinMode(led, OUTPUT);
+ pinMode(u_led, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(my_led, HIGH);
+  Serial.print\n("LED IS ON");
+  delay(1000);
+  digitalWrite(my_led, LOW);
+  Serial.print\n("LED IS OFF");
+  delay(1000);
+  digitalWrite(led, HIGH);
+  Serial.print\n("LED IS ON");
+  delay(1000);
+  digitalWrite(my_led, LOW);
+  Serial.print\n("LED IS OFF");
+  delay(1000);
+  digitalWrite(u_led, HIGH);
+  Serial.print\n("LED IS ON");
+  delay(1000);
+  digitalWrite(my_led, LOW);
+  Serial.print\n("LED IS OFF");
+  delay(1000);
+  
+}
